9de5941f0170f2f27aae4644b8b338fa2c35d06f
sara-aires <105974493+sara-aires@users.noreply.github.com>
2024-07-23 16:52:02 -0300

    install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -3,7 +3,7 @@ sudo apt-get update
 sudo apt-get install -y docker.io
 sudo systemctl start docker
 sudo systemctl enable docker
-sudo usermod -aG docker ${USER}
-curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo usermod -aG docker $saraaires
+curl -L "" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 sudo mv /usr/local/bin/docker-compose /usr/bin/docker-compose
